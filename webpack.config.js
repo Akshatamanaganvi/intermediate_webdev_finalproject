4287dd0d882a2a8aef778956203a4a5e2479bd19
Akshata B Managanvi <135851838+Akshatamanaganvi@users.noreply.github.com>
2026-01-07 18:05:03 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,30 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-    mode: "development",
-    entry: "./src/script.js",
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/index.html',
-            favicon: "./src/favicon.ico",
-        })
+  mode: "production",
+
+  entry: "./src/script.js",
+
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      favicon: "./src/favicon.ico",
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ],
-    output: {
-        clean: true,
-        libraryTarget: 'window'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(css)$/i,
-                type: "asset/resource",
-                generator: {
-                    filename: "[name][ext]",
-                },
-            },
-        ],
-    },
+  },
 };
